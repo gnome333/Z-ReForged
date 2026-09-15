91f751ab3fce8b95d969618f5dd9ee9b8ad79039
gnome333 <azegnomik6@gmail.com>
2026-09-15 22:23:51 +0500

    m4a1 u[date

--- lua/weapons/weapon_m4a1.lua
+++ lua/weapons/weapon_m4a1.lua
@@ -13,13 +13,27 @@ SWEP.SlotPos = 10
 SWEP.ViewModel = ""
 SWEP.WorldModel = "models/weapons/w_rif_m4a1.mdl"
 
-SWEP.WepSelectIcon2 = Material("vgui/hud/tfa_ins2_m4a1.png")
-SWEP.IconOverride = "entities/arc9_eft_m4a1.png"
+SWEP.WepSelectIcon2 = Material("entities/arc9_ump45_ins1_m4.png")
+SWEP.IconOverride = "entities/arc9_ump45_ins1_m4.png"
 
 SWEP.Primary.Wait = 0.063
 SWEP.Primary.Automatic = true
 
+SWEP.ZoomPos = Vector(0.20, 0.28, 4.8)
+
+SWEP.LocalMuzzlePos = Vector(20,0.4,2)
+SWEP.LocalMuzzleAng = Angle(0.1,-0.1,-1)
+SWEP.WeaponEyeAngles = Angle(0,3,0)
+
 //SWEP.StartAtt = {"ironsight2", false}
-SWEP.StartAtt = {"ironsight2"}
+-- SWEP.StartAtt = {"ironsight2"}
+
+SWEP.FakeBodyGroups = "0020000"
+
+function SWEP:ModelCreated(model)
+	if CLIENT and IsValid(self:GetWM()) and isstring(self.FakeBodyGroups) then
+		self:GetWM():SetBodyGroups(self.FakeBodyGroups)
+	end
+end
 
 SWEP.cameraShakeMul = 1
